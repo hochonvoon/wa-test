9a97279a213935b864a3d378e3f8da52e6b8a34a
hochonvoon <hochonvoon@gmail.com>
2025-10-28 11:41:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json()); // 允许解析 JSON Body
+
+// GET 测试
 app.get('/', (req, res) => {
   res.send('Hello World 👋');
 });
 
-// Render 需要使用这个 PORT
+// POST 测试
+app.post('/webhook', (req, res) => {
+  console.log('Received:', req.body);
+
+  res.json({
+    reply: `你说了：“${req.body.message}”`,
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Running on ${PORT}`));
